4a5755503686c5264d8adf5ff23d6f2c47dbb991
Mhmd.H <dr.co0obra@hotmail.com>
2021-04-07 19:03:35 +0300

    final

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,19 +1,72 @@
-import React from 'react'
-import {useSelector,useDispatch} from 'react-redux'
-import { Grid } from '@material-ui/core'
-import {makeStyles} from '@material-ui/styles'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import { startUploadImage } from '../actions/utilActions';
+import { AppState } from '../reducers';
 const Homepage = () => {
-    const useStyles = makeStyles((theme) => ({
-        root:{
-            minHeight:'100vh'
-        }
-	}));
-    const classes=useStyles()
-    return (
-        <Grid container justify='center' alignItems='center' className={classes.root}>
-            <h2>Hello world</h2>
-        </Grid>
-    )
-}
+  const dispatch = useDispatch();
+  const StateImage = useSelector((state: AppState) => state.util.image);
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      minHeight: '100vh',
+      paddingTop: '50px',
+    },
+    image: {
+      height: 300,
+      width: 300,
+    },
+  }));
 
-export default Homepage
+  const classes = useStyles();
+  return (
+    <Container maxWidth="lg" className={classes.root}>
+      <Grid container justify="center" alignItems="center" direction="column">
+        <Typography variant="h2">Upload your image</Typography>
+        <TextField
+          type="file"
+          id="image-input"
+          variant="outlined"
+          onChange={(event: any) => {
+            event.target.files[0] &&
+              dispatch(
+                startUploadImage(URL.createObjectURL(event.target.files[0]))
+              );
+          }}
+        />
+      </Grid>
+
+      <Grid
+        item
+        container
+        justify="center"
+        style={{ marginTop: '50px' }}
+        direction="column"
+      >
+        <img
+          src={StateImage}
+          alt=""
+          id="my-image"
+          className={classes.image}
+          hidden
+        />
+        <canvas id="canvas" width="400px" height="200px" />
+        {StateImage !== '' && (
+          <Button variant="contained">
+            <a href={StateImage} download>
+              Download
+            </a>
+          </Button>
+        )}
+      </Grid>
+    </Container>
+  );
+};
+
+export default Homepage;
--- src/actions/utilActions.ts
+++ src/actions/utilActions.ts
@@ -1,12 +1,31 @@
-import {AppState} from '../reducers/index'
-import {Dispatch} from 'redux'
-import {AppActions} from '../models'
-export const CreatePost = (image:string): AppActions => ({
-	type: 'UPLOAD_IMAGE',
-	image
+import { AppState } from '../reducers/index';
+import { Dispatch } from 'redux';
+import { AppActions } from '../models';
+export const uploadImage = (image: string): AppActions => ({
+  type: 'UPLOAD_IMAGE',
+  image,
 });
-export const startUploadImage = (e: Number) => {
-	return (dispatch: Dispatch<AppActions>, getState: () => AppState) => {
-	
-	};
+export const startUploadImage = (image: string) => {
+  return (dispatch: Dispatch<AppActions>, getState: () => AppState) => {
+    let img = new Image();
+    img.src = image;
+    img.onload = function () {
+      const text: string = 'Hello World';
+      const canvas: HTMLCanvasElement = document.getElementById(
+        'canvas'
+      ) as HTMLCanvasElement;
+      const myContext = canvas.getContext('2d');
+      const myImage = document.getElementById('my-image') as HTMLImageElement;
+      canvas.width = 600;
+      canvas.height = 600;
+      myContext!.drawImage(myImage, 0, 0);
+      myContext!.font = '38px verdana';
+      myContext!.globalAlpha = 0.5;
+      myContext!.fillStyle = 'red';
+      myContext!.fillText(text, 200, 300);
+      myContext!.fillStyle = 'green';
+      myContext!.fillText(text, 200, 300);
+    };
+    dispatch(uploadImage(image));
+  };
 };
