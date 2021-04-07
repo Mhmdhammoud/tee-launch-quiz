f02813fe4431299a4ef5a7a3ba575ed481fbe5e7
Mhmd.H <dr.co0obra@hotmail.com>
2021-04-07 19:05:17 +0300

    Small Changes: 1- Added .vscode config folder to gitignore 2- Small store changes

--- src/redux-store-config/index.ts
+++ src/redux-store-config/index.ts
@@ -1,34 +1,29 @@
-import {applyMiddleware, compose, createStore} from 'redux';
-import thunk, {ThunkMiddleware} from 'redux-thunk';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk, { ThunkMiddleware } from 'redux-thunk';
 import monitorReducersEnhancer from '../enhancers/monitor';
 import loggerMiddleware from '../middlewares/logger';
-import {AppActions} from '../models';
-import rootReducer, {AppState} from '../reducers';
-import {composeWithDevTools} from 'redux-devtools-extension';
-
-export interface IAuthState {
-	isAuthenticated?: boolean;
-	user?: any;
-}
+import { AppActions } from '../models';
+import rootReducer, { AppState } from '../reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const configStore = () => {
-	const middlewares = [
-		loggerMiddleware,
-		thunk as ThunkMiddleware<AppState, AppActions>,
-	];
-	const middlewareEnhancer = applyMiddleware(...middlewares);
-	const enhancers = [middlewareEnhancer, monitorReducersEnhancer];
-	const composedEnhancers: any = compose(...enhancers);
-	const store = createStore(
-		rootReducer,
-		composeWithDevTools(composedEnhancers)
-	);
-	if (process.env.NODE_ENV !== 'production' && (module as any).hot) {
-		(module as any).hot.accept('../reducers', () =>
-			store.replaceReducer(rootReducer)
-		);
-	}
+  const middlewares = [
+    loggerMiddleware,
+    thunk as ThunkMiddleware<AppState, AppActions>,
+  ];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+  const enhancers = [middlewareEnhancer, monitorReducersEnhancer];
+  const composedEnhancers: any = compose(...enhancers);
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(composedEnhancers)
+  );
+  if (process.env.NODE_ENV !== 'production' && (module as any).hot) {
+    (module as any).hot.accept('../reducers', () =>
+      store.replaceReducer(rootReducer)
+    );
+  }
 
-	return store;
+  return store;
 };
 export default configStore;
